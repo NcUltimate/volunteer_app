41f539fe9e78b3dfd35eb088d461690373343443
unknown <BPorter@IN5580.ET.LOCAL>
2014-06-28 15:20:46 -0400

    Fix broken logout

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  req.session.loggedIn = false;
   res.render('login', { title: 'Login' });
 });
 router.post('/', function(req, res) {
