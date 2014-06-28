f12dc08d5013f0c3acdadd86bb6eb63432e72d8f
Joseph Klemen <jmklemen@exacttarget.com>
2014-06-28 12:03:09 -0400

    changed some styles on filters.

--- routes/filter.js
+++ routes/filter.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('filter', { title: 'Filter' });
+	var filters = {division: ['Professional', 'Collegiate', 'High School'],
+					indoors: ['Outdoors', 'Indoors'],
+					time_of_day: ['Morning', 'Afternoon', 'Evening', 'Night'],
+					day_of_week: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+				  }
+  res.render('filter', { title: 'Filter', filters: filters });
 });
 
 module.exports = router;
