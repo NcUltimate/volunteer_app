dfc09c9ee8f14ffcda28fc38495d66f833321b0a
anthonynatoli <anatoli@purdue.edu>
2014-06-28 14:07:41 -0400

    Gets filters on save click

--- public/javascripts/filterpage.js
+++ public/javascripts/filterpage.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-          $('#filterview').slideToggle(300);
+          //$('#filterview').slideToggle(300);
+          console.log("page ready");
+          $( '#save' ).click(clicked);
 	});
+
+function clicked(){
+	console.log("Save pressed");
+	var selected = [];
+	$("input:checkbox[name=filters]:checked").each( function(){
+		selected.push( this.id );
+	});
+	return selected;
+}
